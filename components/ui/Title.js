cc7be0c635fee796212954645f8e15b5e09ff261
Djordje Arsenovic <dj.arsenovic@gmail.com>
2024-06-30 17:37:21 +0200

    Showing how we can code different things for iOS and android.

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,21 +1,23 @@
-import { StyleSheet, Text } from 'react-native';
-export default function Title({ children }) {
-  return (
-    <Text style={styles.title}>
-      {children}
-    </Text>
-  )
+import {StyleSheet, Text, Platform} from 'react-native';
+
+export default function Title({children}) {
+    return (
+        <Text style={styles.title}>
+            {children}
+        </Text>
+    )
 }
 
 const styles = StyleSheet.create({
-  title: {
-    fontFamily: 'open-sans-bold',
-    fontSize: 24,
-    color: 'white',
-    maxWidth: '80%',
-    textAlign: 'center',
-    padding: 12,
-    borderWidth: 2,
-    borderColor: 'white'
-  }
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 24,
+        color: 'white',
+        maxWidth: '80%',
+        textAlign: 'center',
+        padding: 12,
+        // borderWidth: Platform.OS === 'ios' ? 0 : 2,
+        borderWidth: Platform.select({ios: 0, android: 2}),
+        borderColor: 'white'
+    }
 })
